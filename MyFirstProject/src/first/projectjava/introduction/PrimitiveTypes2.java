a569627afa9b53ddd48e9faddc26b8c54b94247d
Lucas Rodrigues da Silva <89489811+LucasRodriguesSilva@users.noreply.github.com>
2021-09-11 15:40:10 -0300

    Add files via upload

---
+++ MyFirstProject/src/first/projectjava/introduction/PrimitiveTypes2.java
@@ -0,0 +1,8 @@
+package first.projectjava.introduction;
+
+public class PrimitiveTypes2 {
+    public static void main(String[] args) {
+        int age = 10;
+        System.out.println("The age is "+age+" years old!");
+    }
+}
