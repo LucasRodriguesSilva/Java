3ddec234dd5aef1271747c1ff628367ea087f118
Lucas Rodrigues da Silva <89489811+LucasRodriguesSilva@users.noreply.github.com>
2021-09-11 13:04:46 -0300

    Add files via upload

---
+++ MyFirstProject/src/first/projectjava/introduction/MyFirstProject.java
@@ -0,0 +1,15 @@
+package first.projectjava.introduction;
+
+/**
+ * Este é um comentário <b>javadoc</b>
+ */
+public class MyFirstProject {
+    public static void main(String[] args){
+        //Este é um comentário de uma linha
+        /* Este é um comentário
+        de múltiplas
+        linhas
+         */
+        System.out.println("Oi, pessoal");
+    }
+}
